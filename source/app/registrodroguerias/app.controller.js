e4c78d4f4c22c592c86e9c884253efb8327eb3ef
maasprilla35 <maasprilla35@misena.edu.co>
2016-04-10 23:17:37 -0500

    se añadieron los mensajes de alerta al crear usuarios y droguerias

--- source/app/registrodroguerias/app.controller.js
+++ source/app/registrodroguerias/app.controller.js
@@ -5,8 +5,8 @@
   ]).controller('drogueriaCreateCtrl', drogueriaCreateCtrl);
 
 
-  drogueriaCreateCtrl.$inject = ['Usuarios', 'Ciudades'];
-  function drogueriaCreateCtrl(Ususarios,Ciudades){
+  drogueriaCreateCtrl.$inject = ['$location', '$mdToast', 'Usuarios', 'Ciudades'];
+  function drogueriaCreateCtrl($location, $mdToast, Ususarios,Ciudades){
 
     var vm=this;
     vm.ciudades=Ciudades.query();
@@ -17,7 +17,13 @@
         vm.idciudades=getIdCiudad(vm.ciudad)
         vm.usuario.ciudad={ciudadPK:{idCiudad:vm.idciudades[0], iddepartamento:vm.idciudades[1]}};
         vm.usuario.idRol={idRol:2};
-        Ususarios.save(vm.usuario);
+        Ususarios.save(vm.usuario , function() {
+                $location.path('/');
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('Se ha  guardado el Usuario...')
+                        .position('bottom right'));
+        });
     }
 
 
--- source/app/registrousuarios/app.controller.js
+++ source/app/registrousuarios/app.controller.js
@@ -5,8 +5,8 @@
   ]).controller('usuariosCreateCtrl', usuariosCreateCtrl);
 
 
-  usuariosCreateCtrl.$inject = ['Usuarios', 'Ciudades'];
-  function usuariosCreateCtrl(Ususarios,Ciudades){
+  usuariosCreateCtrl.$inject = ['$location', '$mdToast','Usuarios', 'Ciudades'];
+  function usuariosCreateCtrl($location, $mdToast, Ususarios ,Ciudades){
 
     var vm=this;
     vm.ciudades=Ciudades.query();
@@ -17,7 +17,13 @@
         vm.idciudades=getIdCiudad(vm.ciudad)
         vm.usuario.ciudad={ciudadPK:{idCiudad:vm.idciudades[0], iddepartamento:vm.idciudades[1]}};
         vm.usuario.idRol={idRol:1};
-        Ususarios.save(vm.usuario);
+        Ususarios.save(vm.usuario, function() {
+                $location.path('/');
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('Se ha  guardado el Usuario...')
+                        .position('bottom right'));
+            });
     }
 
 
@@ -26,7 +32,6 @@
 
   function getIdCiudad(id){
     var array = id.split('-');
-    console.log(array.length)
     return array;
   }
 
