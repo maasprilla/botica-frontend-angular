54bbd1018a175ff00b03fa507256d48aa9d204a3
joseHG44 <hjhenao@misena.edu.co>
2016-04-27 11:10:49 -0500

    se realiza formulario de registo de administradores y se le aplica estilos con responsive

--- source/app/config/app.constantes.js
+++ source/app/config/app.constantes.js
@@ -4,7 +4,7 @@
   angular.module('app.config',[
 
   ]).constant('BASEURL',
-    'http://localhost:8080/botica-backend-java/webresources')
+    'http://localhost:31409/botica-backend-java/webresources')
     .config(configure);
 
     configure.$inject = ['$authProvider', 'BASEURL'];
