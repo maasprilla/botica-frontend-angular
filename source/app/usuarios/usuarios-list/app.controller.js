37c2c226ba07a2ff4ac7411cada3a4ff22338590
davemeister <yaryanbinks@hotmail.com>
2016-04-27 22:56:28 -0500

    Tablas y formularios con responsive design, mejoras en diseño

--- source/app/usuarios/usuarios-list/app.controller.js
+++ source/app/usuarios/usuarios-list/app.controller.js
@@ -10,20 +10,15 @@
 
     var vm=this;
     vm.usuarios=Usuarios.query();
+    vm.idciudades;
 
 
-    /** vm.create=function(){
+    vm.create=function(){
         vm.idciudades=getIdCiudad(vm.ciudad)
         vm.usuario.ciudad={ciudadPK:{idCiudad:vm.idciudades[0], iddepartamento:vm.idciudades[1]}};
         vm.usuario.idRol={idRol:1};
-        Ususarios.save(vm.usuario, function() {
-                $location.path('/');
-                $mdToast.show(
-                    $mdToast.simple()
-                        .textContent('Se ha  guardado el Usuario...')
-                        .position('bottom right'));
-            });
-    } **/
+
+    }
 
 
 
