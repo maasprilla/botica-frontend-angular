9527939b72a573b15ec0acc56052e7100c16c202
ingrid9709 <ingrid9709@hotmail.com>
2016-06-13 19:46:35 -0500

    commit

--- source/app/config/app.constantes.js
+++ source/app/config/app.constantes.js
@@ -6,8 +6,6 @@
   ]).constant('BASEURL',
 
 
-    // 'http://localhost:31833/botica-backend-java/webresources')
-    
     'http://localhost:8080/botica-backend-java/webresources')
 
     .config(configure);
