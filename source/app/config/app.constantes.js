9d16ba32edbe1366c0f0534e9eb0ac5b630e1ef1
davemeister <yaryanbinks@hotmail.com>
2016-06-16 00:59:49 -0500

    se corrigieron detalles, se hizo responsive en la tabla de lista de precios

--- source/app/config/app.constantes.js
+++ source/app/config/app.constantes.js
@@ -6,13 +6,10 @@
   ]).constant('BASEURL',
 
 
-    'http://localhost:8080/botica-backend-java/webresources')
-
     // 'http://localhost:31833/botica-backend-java/webresources')
   //  'http://localhost:43096/botica-backend-java/webresources')
-    // 'http://localhost:8080/botica-backend-java/webresources')
+  'http://localhost:8080/botica-backend-java/webresources')
     // 'http://localhost:43096/botica-backend-java/webresources')
-    'http://localhost:8080/botica_backend/webresources')
 
     .config(configure);
 
